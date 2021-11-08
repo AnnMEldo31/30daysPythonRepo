ac00255d610cd2438d2b29ff4c3e7027ec956e66
AnnMEldo31 <33516189+AnnMEldo31@users.noreply.github.com>
2021-11-08 09:26:14 +0000

    added to replit branch

---
+++ day2.py
@@ -0,0 +1,21 @@
+name = input("Please enter your name: ")
+age = input("Please enter your age: ")
+print(name)
+print(age)
+
+number = 32
+print(number)
+number = 26
+print(number)
+number = input("Enter a number: ")
+print(number)
+
+hourly_wage = input("Please enter your hourly wage: ")
+
+print("Hourly wage: ")
+print(hourly_wage)
+
+hours_worked = input("How many hours did you work this week? ")
+
+print("Hours worked: ")
+print(hours_worked)
